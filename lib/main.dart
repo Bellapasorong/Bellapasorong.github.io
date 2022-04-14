0c2d59e9809fa607349b1d75a87d94da919957aa
Bellapasorong <86918789+Bellapasorong@users.noreply.github.com>
2022-04-14 14:08:05 +0800

    151_Hilary Bella_P2

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter_application_1/MainPage.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MainPage(),
+      debugShowCheckedModeBanner: false,
+      
+    );
+  }
+}
